67e40bb25753789a7e6736e5e415e856c63b9fc6
Vasiliev Sergei <sergei.binp@gmail.com>
2023-06-05 17:06:46 +0500

    final commit

--- tasks/Program.cs
+++ tasks/Program.cs
@@ -215,6 +215,7 @@ void AmplifyMatrix(int[,] arrayA, int[,] arrayB)
 // 27(0,0,1) 90(0,1,1)
 // 26(1,0,1) 55(1,1,1)
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 int[,,] Create3DArray(int rows, int columns, int stages)
 {
     int[,,] array = new int[rows, columns, stages];
